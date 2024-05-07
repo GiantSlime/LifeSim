d5c02825e11a5d1ea93ccd8c4d4d3f41dacb495a
Norms <yidzkylune@gmail.com>
2024-05-07 14:31:17 +1200

    Added tooltip system

---
+++ Assets/Scripts/TooltipTrigger.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	[Multiline]
+	public string content;
+
+	public string header;
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		Debug.Log("TootipTrigger:OnPointerEnter");
+		TooltipSystem.Show(content, header);
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		Debug.Log("TootipTrigger:OnPointerExit");
+		TooltipSystem.Hide();
+	}
+
+	public void OnMouseEnter()
+	{
+		Debug.Log("TootipTrigger:OnMouseEnter");
+		TooltipSystem.Show(content, header);
+	}
+
+	private void OnMouseExit()
+	{
+		Debug.Log("TootipTrigger:OnMouseEnter");
+		TooltipSystem.Hide();
+	}
+}
